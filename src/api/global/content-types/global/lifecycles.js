d595877032adff57d62e5ebaf1551344dc6468dd
salahdev667 <166670991+salahdev667@users.noreply.github.com>
2024-04-11 03:58:54 +0000

    Update lifecycles.js

--- src/api/global/content-types/global/lifecycles.js
+++ src/api/global/content-types/global/lifecycles.js
@@ -2,14 +2,14 @@ module.exports = {
     async afterUpdate(event) {    // Connected to "Save" button in admin panel
         const { result } = event;
 
-      console.log(event)
+      console.log(result.siteDecription)
 
         try{
             await strapi.plugins['email'].services.email.send({
               to: 'salaheddine.boulahya@gmail.com',
               from: 'no-reply@strapiapp.com', // e.g. single sender verification in SendGrid
               subject: 'The Strapi Email plugin worked successfully',
-              html: result.siteDescription, 
+              text: result.siteDecription, 
                 
             })
         } catch(err) {
