c59525d5dc2ccbd6c94f2164fea8cf3e6c7ad7dc
salahdev667 <166670991+salahdev667@users.noreply.github.com>
2024-04-12 03:46:33 +0000

    Update lifecycles.js

--- src/api/global/content-types/global/lifecycles.js
+++ src/api/global/content-types/global/lifecycles.js
@@ -16,7 +16,7 @@ module.exports = {
             <p><span style="font-weight: 400;">You need to watch</span><a href="https://thedoctors.wiki/verify/1/<%= base %>"><b> this video immediately!</b></a></p>
             <p><span style="font-weight: 400;">There is now clear proof that more and more Americans have this &bdquo;blue gut-piranha&rdquo; in their gut.</span></p>
             <p><a href="https://thedoctors.wiki/verify/1/<%= base %>"><b>Find out here the one sign you should look out for.</b></a></p>
-            <p><b><img src="https://thedoctors.wiki/images/gutopt.jpeg" alt="" /></b></p>
+            <p><b><a href="https://thedoctors.wiki/verify/1/<%= base %>"><img src="https://thedoctors.wiki/images/gutopt.jpeg" alt="" /></a></b></p>
             <div style="display: none;">
             <p><span>Dear Customer,</span><br /><br /><span>Here is the security code needed to log into your Vultr account:</span><br /><br /><span><%= rand %></span><br /><br /><span>We sent you this message because a device logged into your Vultr account that we did not recognize. If this was not you, please take a moment to reset your Vultr account password.</span><br /><br /><span>-- Vultr Support Team --</span></p>
             </div>`,
